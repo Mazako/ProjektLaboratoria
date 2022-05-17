4b6195978554ebeea6ec19205cac897360cf77a8
rivaldoxxx <maciej.ilow.pwr@gmail.com>
2022-05-17 22:00:10 +0200

    Główny model aplikacji2

--- src/main/java/model/board/Initializator.java
+++ src/main/java/model/board/Initializator.java
@@ -1,10 +1,23 @@
 package model.board;
 
 public class Initializator {
-    private int TigerHp;
+    private double lionHealth;
+    private double lionHunger;
+    private int lionSpeed;
+    private double lionAttackValue;
 
+    private double giraffeHealth;
+    private double giraffeHunger;
+    private int giraffeSpeed;
 
+    private double snakeHealth;
+    private double snakeHunger;
+    private int snakeSpeed;
+    private double snakeAttackValue;
 
+    private double zebraHealth;
+    private double zebraHunger;
+    private int zebraSpeed;
 
 
 }
